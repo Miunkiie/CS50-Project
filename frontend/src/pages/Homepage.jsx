f81dd4277a37f75b934b35e47e0f84c30573b7b6
Miunkiie <Danuwangu25@gmail.com>
2023-01-31 21:20:18 +1100

    minor change to banner

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,27 +3,34 @@ import Carousel from "../components/carousel/Carousel"
 import CarouselItem from '../components/carousel/CarouselItem'
 import { RxDividerHorizontal } from "react-icons/rx"
 import placeholder from '../assets/images/placeholder.jpg'
+import Banner from "../components/banner/Banner"
 
 function Homepage() {
   return (
-    <div className="container">
-      <img src={clearanceBanner} alt="Huge Clearance Sale" className="hero-banner" />
-      <section className="heading">
-        <h2 className="section-heading">
-          <RxDividerHorizontal className="section-divider" />
-          New Arrivals
-          <RxDividerHorizontal className="section-divider"/>
-        </h2>
-      </section>
-      <Carousel>
-        <CarouselItem image={placeholder} alt={""} />
-        <CarouselItem image={placeholder} alt={""} />
-        <CarouselItem image={placeholder} alt={""} />
-        <CarouselItem image={placeholder} alt={""} />
-        <CarouselItem image={placeholder} alt={""} />
-        <CarouselItem image={placeholder} alt={""} />
-        <CarouselItem image={placeholder} alt={""} />
-      </Carousel>
+    <div>
+      <Banner>
+        <b>Free shipping for orders above $49.99!! Australia only!!</b>
+        <b>International Shipping</b>
+      </Banner>
+      <div className="container">
+        <img src={clearanceBanner} alt="Huge Clearance Sale" className="hero-banner" />
+        <section className="heading">
+          <h2 className="section-heading">
+            <RxDividerHorizontal className="section-divider" />
+            New Arrivals
+            <RxDividerHorizontal className="section-divider"/>
+          </h2>
+        </section>
+        <Carousel>
+          <CarouselItem image={placeholder} alt={""} />
+          <CarouselItem image={placeholder} alt={""} />
+          <CarouselItem image={placeholder} alt={""} />
+          <CarouselItem image={placeholder} alt={""} />
+          <CarouselItem image={placeholder} alt={""} />
+          <CarouselItem image={placeholder} alt={""} />
+          <CarouselItem image={placeholder} alt={""} />
+        </Carousel>
+      </div>
     </div>
   )
 }
