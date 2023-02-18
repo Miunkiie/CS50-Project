e5f4bdc8c260da2f45fe2c80851e47cf2e35c147
Miunkiie <Danuwangu25@gmail.com>
2023-02-18 17:35:48 +1100

    Refactored

--- frontend/src/components/collectionItem/CollectionItem.jsx
+++ frontend/src/components/collectionItem/CollectionItem.jsx
@@ -1,10 +1,11 @@
 import './collectionItem.css'
 
-function CollectionItem({img, alt, description, price}) {
+function CollectionItem({item, alt}) {
+  const {images, description, price} = item
   return (
     <div className="collection-item">
         <div className="item-image">
-            <img src={img} alt={alt} />
+            <img src={images} alt={alt} />
             <div className="item-footer">
               <h3>{description}</h3>
               <h4>{price}</h4>
