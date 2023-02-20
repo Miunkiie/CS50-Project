2077e7873549a09f8876ae0df4b267a1e6bc82a9
Miunkiie <Danuwangu25@gmail.com>
2023-02-20 21:39:09 +1100

    turned CarouselItem into a HOC

--- frontend/src/components/carousel/CarouselItem.jsx
+++ frontend/src/components/carousel/CarouselItem.jsx
@@ -1,6 +1,13 @@
-function CarouselItem({image, alt}) {
+import placeholder from '../../assets/images/placeholder.jpg'
+
+function CarouselItem({item}) {
+  const { description, image } = item
+
   return (
-    <img src={image} alt={alt}/>
+    <a href="">
+        <img src={image} alt={description} />
+    </a>
+
   )
 }
 export default CarouselItem
