d1e508b28defd8fd4e8a296f204d975471d1ef3a
Miunkiie <Danuwangu25@gmail.com>
2023-03-13 17:52:25 +1100

    Added subcategories for each gender

---
+++ frontend/src/assets/categories/categories.js
@@ -0,0 +1,30 @@
+const men = {
+    Outwear: [
+        "Jacket", "Hoodie"
+    ],
+    Bottom: [
+        "Pants", "Shorts"
+    ],
+    Top: [
+        "Long-sleeves", "Shirts"
+    ]
+}
+
+const women = {
+    Outwear: [
+        "Jacket", "Hoodie"
+    ],
+    Bottom: [
+        "Skirts", "Shorts", "Pants"
+    ],
+    Top: [
+        "Long-sleeves", "Shirts", "Crop-Top"
+    ]
+}
+
+const categories = {
+    men,
+    women
+}
+
+export default categories
