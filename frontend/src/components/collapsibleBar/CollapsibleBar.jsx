d696e455a833c73fd5961acba0a17c24f7fc6d58
Miunkiie <Danuwangu25@gmail.com>
2023-04-22 18:45:06 +1000

    isolated component to render categories

--- frontend/src/components/collapsibleBar/CollapsibleBar.jsx
+++ frontend/src/components/collapsibleBar/CollapsibleBar.jsx
@@ -1,30 +1,36 @@
-import { Link, useParams } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useState, useRef } from 'react'
 
 import { SlArrowDown } from 'react-icons/sl'
 
 import './CollapsibleBar.css'
 
-function CollapsibleBar({categories, subCategory}) {
-  const [open, setOpen] = useState(false)
-  const {gender} = useParams()
+function CollapsibleBar({ categories, subCategory, setCategory }) {
+  const [open, setOpen] = useState(false)  
+  const isActive = useRef(null)
 
-  const toggle = useRef(null)    
+  const changeCategory = (e) => {
+    setCategory(prevState => ({
+      ...prevState,
+      category: e.target.dataset.category.toLowerCase()
+    }))
+  }
 
-  const category = subCategory.map(category => 
-    <Link to={`/collections/${gender}/${category.toLowerCase()}`}
-      reloadDocument key={category} value={category}>
+  // Maps the subcategories
+  const subCategories = subCategory.map(category => 
+    <NavLink ref={isActive} onClick={changeCategory} data-category={category} 
+    to={`${category.toLowerCase()}`} key={category}>
       {category}
-    </Link>
+    </NavLink>
   )
-  
+
   return (
     <div className='collapsible-bar'>
-        <button ref={toggle} onClick={() => setOpen(!open)}>
+        <button onClick={() => setOpen(!open)}>
           {categories}
           <SlArrowDown />
         </button>
-        {open && category}
+        {open && subCategories}
     </div>
   )
 }
