daa0b27199e5a2d535f79501500652e394d76ebf
Miunkiie <Danuwangu25@gmail.com>
2023-05-08 22:00:20 +1000

    Refactored code to allow different sidebar options. Links or checkboxes

--- frontend/src/components/collapsibleBar/CollapsibleBar.jsx
+++ frontend/src/components/collapsibleBar/CollapsibleBar.jsx
@@ -1,35 +1,10 @@
-import { NavLink } from 'react-router-dom'
-import { useState } from 'react'
-
-import { SlArrowDown } from 'react-icons/sl'
-
 import './CollapsibleBar.css'
 
-function CollapsibleBar({ categories, subCategory, setCategory }) {
-  const [open, setOpen] = useState(false)  
-
-  const changeCategory = (e) => {
-    setCategory(prevState => ({
-      ...prevState,
-      category: e.target.dataset.category.toLowerCase()
-    }))
-  }
-
-  // Maps the subcategories
-  const subCategories = subCategory.map(category => 
-    <NavLink onClick={changeCategory} data-category={category} 
-    to={`${category.toLowerCase()}`} key={category}>
-      {category}
-    </NavLink>
-  )
+function CollapsibleBar({ children }) {
 
   return (
     <div className='collapsible-bar'>
-        <button onClick={() => setOpen(!open)}>
-          {categories}
-          <SlArrowDown />
-        </button>
-        {open && subCategories}
+      { children }
     </div>
   )
 }
