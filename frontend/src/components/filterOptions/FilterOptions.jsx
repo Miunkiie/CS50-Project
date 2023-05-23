ce160bd9b11029720cb5eaa957de8a3f9e906531
Miunkiie <Danuwangu25@gmail.com>
2023-05-23 23:57:46 +1000

    Function that adds/deletes to/from an array of active filters

--- frontend/src/components/filterOptions/FilterOptions.jsx
+++ frontend/src/components/filterOptions/FilterOptions.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 import './filterOptions.css'
 import { SlArrowDown } from 'react-icons/sl'
 
 
-function FilterOptions({ setFilters, category, subCategory }) {
+function FilterOptions({ filter, options }) {
   const [open, setOpen] = useState(false)
+  const [activeFilters, setActiveFilters] = useState({})
+  const [searchParams, setSearchParams] = useSearchParams()
 
-  // Add filtered options
+  // Create an array of active filters and set search params to that.
   const updateFilters = (e) => {
-    setFilters(prevState => ({
-        ...prevState,
-        category: e.target.value.toLowerCase()
-    }))
+    const {value, checked, name} = e.target
+    const filterCategory = activeFilters[name]
+
+    // Check if filter is already in array or not.
+    if (checked) {
+      // Add the filter if it doesn't exist in the array of active filters
+      if (!filterCategory) {
+        setActiveFilters(prevState => ({
+          ...prevState,
+          [name]: [value.toLowerCase()]
+        }))
+      } else {
+        filterCategory.push(value.toLowerCase())
+      }
+
+    } else {
+      // find the index of the existing array then remove it
+      const index = filterCategory.indexOf(value.toLowerCase())
+      if (index > -1) {
+        filterCategory.splice(index, 1)
+      }
+
+      // Remove category if there are no options selected for it
+      if (filterCategory.length === 0) {
+        delete activeFilters[name]
+      }
+    }
   }
 
-  const subCategories = subCategory.map(category =>
-    <label className="form-control" key={category}> 
-      <input type="checkbox" name={category} onClick={updateFilters} value={category} />
-      {category}
+  const filterOptions = options.map(option =>
+    <label className="form-control" key={option}> 
+      <input type="checkbox" name={filter.toLowerCase()} onClick={updateFilters} value={option} />
+      {option}
     </label>
   )
+
+  // useEffect(() => {
+  //   console.log(activeFilters)
+  // }, [activeFilters, updateFilters])
 
   return (
     <>
       <button onClick={() => setOpen(!open)}>
-        {category}
+        {filter}
       <SlArrowDown />
       </button>
-      { open && subCategories }
+      { open && filterOptions }
     </>
   )
 }
