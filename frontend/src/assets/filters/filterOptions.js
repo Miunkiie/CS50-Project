36786bba8e816a3d732485a061849054c001561b
Miunkiie <Danuwangu25@gmail.com>
2023-05-23 23:56:15 +1000

    Filter options

---
+++ frontend/src/assets/filters/filterOptions.js
@@ -0,0 +1,28 @@
+const filterOptions = {
+    Gender: [
+        "Men",
+        "Women"
+    ],
+    Colors: [
+        "Black",
+        "Grey",
+        "White",
+        "Brown",
+        "Red",
+        "Yellow",
+        "Green",
+        "Beige",
+        "Pink",
+        "Blue"
+    ],
+    Sizes: [
+        "XS",
+        "S",
+        "M",
+        "L",
+        "XL"
+    ]
+
+}
+
+export default filterOptions
