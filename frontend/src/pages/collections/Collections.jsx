381a01952061327797e612ba7a32e9c5d62a76fb
Miunkiie <Danuwangu25@gmail.com>
2023-06-02 21:31:06 +1000

    Decouple dispatch from CollectionOverview

--- frontend/src/pages/collections/Collections.jsx
+++ frontend/src/pages/collections/Collections.jsx
@@ -1,18 +1,32 @@
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector }  from 'react-redux'
+import { toast } from 'react-toastify'
 
 import CollectionOverview from "../../components/collectionOverview/CollectionOverview"
 import SideBarLink from '../../components/sideBarLink/SideBarLink'
 import categories from '../../assets/categories/categories'
+import { getProducts } from "../../features/product/productSlice"
 
 function Collections() {
     // Retrieve path params to each collectionOverview
+    const dispatch = useDispatch()
+    const { isError, message } = useSelector(state => state.product)
+
     const {gender, category} = useParams();
 
     const [filters, setFilters] = useState({
     gender: gender || "",
     category: category || "",
   })
+
+  useEffect(() => {
+    dispatch(getProducts(filters))
+    
+    if (isError) {
+      toast.error(message)
+    }
+  }, [dispatch, isError, message, filters])
   
   // Renders categories
   const renderedCategories = Object.entries(categories[gender]).map(([key, value]) => 
