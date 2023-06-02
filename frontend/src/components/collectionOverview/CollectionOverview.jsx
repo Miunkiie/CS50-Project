c3d695ef2a89c1d07a39bf445d6cb5856ecaa5f4
Miunkiie <Danuwangu25@gmail.com>
2023-06-02 21:29:20 +1000

    Decoupled dispatch / moved to collections page

--- frontend/src/components/collectionOverview/CollectionOverview.jsx
+++ frontend/src/components/collectionOverview/CollectionOverview.jsx
@@ -1,8 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useCallback } from 'react'
-import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
+import { useCallback } from 'react'
 
-import { getProducts } from "../../features/product/productSlice"
 import CollectionItem from "../../components/collectionItem/CollectionItem"
 import CollapsibleBar from '../collapsibleBar/CollapsibleBar'
 import Sidebar from "../sidebar/Sidebar"
@@ -10,9 +8,8 @@ import SortBy from '../sortBy/SortBy'
 
 import './collectionOverview.css'
 
-function CollectionOverview({filters, setFilters, categories, title, heading}) {
-  const { product, isError, message } = useSelector(state => state.product)
-  const dispatch = useDispatch()
+function CollectionOverview({setFilters, categories, title, heading}) {
+  const { product } = useSelector(state => state.product)
 
   // Renders all products from that category
   const renderedCollection = product.map(item =>
@@ -25,15 +22,6 @@ function CollectionOverview({filters, setFilters, categories, title, heading}) {
       sort: sort
     }))
   }, [setFilters])
-
-
-  useEffect(() => {
-    dispatch(getProducts(filters))
-    
-    if (isError) {
-      toast.error(message)
-    }
-  }, [dispatch, isError, message, filters])
 
   return (
     <div className="collection-container">
