3fc0c1a6d8e8de424fd6d48f909641b51de5a37e
Miunkiie <Danuwangu25@gmail.com>
2023-06-13 23:39:45 +1000

    Init sort in the filters to empty string so we can get the length when checking if empty

--- frontend/src/pages/search/search.jsx
+++ frontend/src/pages/search/search.jsx
@@ -1,11 +1,16 @@
 import { useSearchParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { useDispatch, useSelector }  from 'react-redux'
+import { toast } from 'react-toastify'
 
 import filterOptions from "../../assets/filters/filterOptions"
 import CollectionOverview from "../../components/collectionOverview/CollectionOverview"
 import FilterOptions from '../../components/filterOptions/FilterOptions'
+import { getProducts } from "../../features/product/productSlice"
 
 function Search() {
+  const dispatch = useDispatch()
+  const { isError, message } = useSelector(state => state.product)
   const [searchParams] = useSearchParams()
   const query = searchParams.get("q")
   const [filters, setFilters] = useState({
@@ -13,12 +18,33 @@ function Search() {
   })
 
   const renderedFilters = Object.entries(filterOptions).map(([key, value]) => 
-      <FilterOptions key={key} filter={key} options={value} setFilters={setFilters} />
+    <FilterOptions 
+    key={key}
+    filterHeading={key}
+    options={value}
+    setFilters={setFilters}
+    filters={filters} />
   )
 
+    // Creates a new object for the updated filters
+    // If we update filters, it'll create an infinite loop
+    let updatedFilters = {}
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value.length > 0) {
+        updatedFilters[key] = value;
+      }   
+    })
+
   useEffect(() => {
-    
-  }, [filters])
+    if (isError) {
+      toast.error(message)
+    }
+  }, [isError, message])
+  
+  // Dispatch Filter
+  useEffect(() => {
+    dispatch(getProducts(updatedFilters))
+  }, [dispatch, filters])
 
   return (
     <CollectionOverview categories={renderedFilters} 
