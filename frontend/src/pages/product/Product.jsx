22a2c4899d895a7c216b446472d13bc6f9422a13
Miunkiie <Danuwangu25@gmail.com>
2023-06-28 22:54:47 +1000

    Implemented synced carousel that display the image when a thumbnail is clicked

--- frontend/src/pages/product/Product.jsx
+++ frontend/src/pages/product/Product.jsx
@@ -1,15 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { newArrivals } from '../../features/product/productSlice'
 
-import Carousel from "../../components/carousel/Carousel"
-import CarouselItem from '../../components/carousel/CarouselItem'
+import Slider from "react-slick"
 import Thumbnail from '../../components/thumbnail/Thumbnail'
 import './product.css'
 
 function Product() {
   const {product, isError, message} = useSelector(state => state.product)
   const dispatch = useDispatch()
+
+  const thumbnailSlider = useRef(null)
+  const mainImageSlider = useRef(null)
 
   useEffect(() => {
     dispatch(newArrivals())
@@ -18,12 +20,14 @@ function Product() {
   const thumbailSettings = {
     arrows: false,
     dots: false,
+    focusOnSelect: true,
     infinite: false,
     slidesToShow: 5,
     slidesToScroll: 1,
     swipeToSlide: true,
     vertical: true,
     verticalSwiping: true,
+    asNavFor: mainImageSlider.current
   }
 
   const mainImageSettings = {
@@ -31,17 +35,16 @@ function Product() {
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
+    asNavFor: thumbnailSlider.current
   }
 
   const renderedThumbnail = product.map(item => 
-    <Thumbnail role="button" key={item.id} item={item} />
+    <Thumbnail item={item} key={item.id} />
   )
 
-    const renderedCarousel = product.map(item => 
-    <CarouselItem key={item.id} item={item} />
+    const renderedImage = product.map(item => 
+    <Thumbnail item={item} key={item.id} />
   )
-
-  // useEffect to detect when a thumbnail is clicked then transfer it to the main image
 
   return (
     <div className="product-container">
@@ -49,14 +52,14 @@ function Product() {
         <div className="content-card">
           <div className="product-image-section">
             <div className="thumbnail-section">
-              <Carousel settings={thumbailSettings}>
+              <Slider {...thumbailSettings} ref={thumbnailSlider}>
                 {renderedThumbnail}
-              </Carousel>
+              </Slider>
             </div>
             <div className="main-image">
-              <Carousel settings={mainImageSettings}>
-                {renderedCarousel}
-              </Carousel>
+              <Slider {...mainImageSettings} ref={mainImageSlider}>
+                {renderedImage}
+              </Slider>
             </div>
           </div>
           <div className="info-container">
