768d48141588b84d60e248a54effb69031b5fb41
Miunkiie <Danuwangu25@gmail.com>
2023-07-12 21:37:37 +1000

    Adjustable product star rating

--- frontend/src/components/starRatings/StarRatings.jsx
+++ frontend/src/components/starRatings/StarRatings.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 
+import { BsStarFill } from 'react-icons/bs'
+
+import './starRatings.css'
 
 function StarRatings() {
     const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
 
     // Generate 5 empty stars
-    const starRatings = Array(5).map(star => 
-        <span className="star-rating">***</span>
-    )
+    const starRatings = [...Array(5)].map((star, index) => {
+      index += 1;
+
+      return (
+        <button type="button" key={index} className={index <= (hover || rating) ? "on" : "off"}
+        onClick={() => setRating(index)}
+        onMouseEnter={() => setHover(index)}
+        onMouseLeave={() => setHover(rating)}
+        >
+          <span className="stars"><BsStarFill /></span>
+        </button>
+      )
+    })
 
   return (
     <div className="star-rating">
