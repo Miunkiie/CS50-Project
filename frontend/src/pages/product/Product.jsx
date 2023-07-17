90623395c23fa87572e5cf703e27068c18769bb4
Miunkiie <Danuwangu25@gmail.com>
2023-07-17 23:43:19 +1000

    moved thumbanil + main image to own component

--- frontend/src/pages/product/Product.jsx
+++ frontend/src/pages/product/Product.jsx
@@ -1,42 +1,28 @@
 import { useSelector } from 'react-redux'
-import { useRef, useEffect } from 'react'
+import { useEffect } from 'react'
 import { toast } from 'react-toastify'
 
-import Slider from "react-slick"
 import StarRatings from "../../components/starRatings/StarRatings"
+import ImagePreview from '../../components/imagePreview/ImagePreview'
 
 import './product.css'
 
 function Product() {
   const {product, isError, message} = useSelector(state => state.product)
 
-  const thumbnailSlider = useRef(null)
-  const mainImageSlider = useRef(null)
-
-  const thumbailSettings = {
-    arrows: false,
-    dots: false,
-    focusOnSelect: true,
-    infinite: false,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-    vertical: true,
-    verticalSwiping: true,
-    asNavFor: mainImageSlider.current
-  }
-
-  const mainImageSettings = {
-    dots: false,
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: thumbnailSlider.current
-  }
-
   const renderedImages = product["images"]?.map((value, index) => 
     <img key={index} src={value} alt={product['description']} />
   )
+
+  // Function to capitalize first character of each word
+  // ^\w : first character of the string
+  // | : or
+  // \s\w : first character after whitespace
+  // (^\w|\s\w) Capture the pattern.
+  // g Flag: Match all occurrences.
+  function capitalizeLetter(text) {
+    return text?.replace(/(^\w|\s\w)(\S*)/g, (_,m1,m2) => m1.toUpperCase()+m2.toLowerCase())
+  }
 
   useEffect(() => {
     if (isError) {
@@ -49,30 +35,33 @@ function Product() {
     <div className="product-container">
         breadcrumbs
         <div className="content-card">
-          <section className="product-image-section">
-            <div className="thumbnail-section">
-              <Slider {...thumbailSettings} ref={thumbnailSlider}>
-                {renderedImages}
-              </Slider>
-            </div>
-            <div className="main-image">
-              <Slider {...mainImageSettings} ref={mainImageSlider}>
-                {renderedImages}
-              </Slider>
-            </div>
-          </section>
+          <ImagePreview images={renderedImages} />
           <section className="info-container">
             <div className="product-details">
               <header className="product-title">
-                <h1>{product.name}</h1>
+                <h1>{capitalizeLetter(product.name)}</h1>
               </header>
-              <p className='product-description'>{product.description}</p>
               <div className="span-separation">
-                <span className="product-price">{`$${product.price}.00`}</span>
-                <StarRatings />
+                <div className="ls">
+                  <span className="product-price">
+                    {`$${product.price}.00`}
+                  </span>
+                </div>
+                <div className="rs">
+                  <StarRatings />
+                  <span className="num-reviews">
+                    <a href='/'>
+                      {`(${product.numOfReviews})`}
+                    </a>
+                  </span>
+                </div>
               </div>
+              <p className='product-description'>
+                {product.description}
+              </p>
             </div>
           </section>
+
         </div>
     </div>
   )
