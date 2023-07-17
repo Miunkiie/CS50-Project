a58f2af3abd7dd6021fcf676ad2495a23cdcce96
Miunkiie <Danuwangu25@gmail.com>
2023-07-17 23:42:35 +1000

    Replaced with productCard

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -1,20 +1,18 @@
-import {useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import { toast } from 'react-toastify'
 import Slider from "react-slick"
-import { useNavigate } from "react-router-dom"
 import { newArrivals } from '../../features/product/productSlice'
-import { getProduct } from '../../features/product/productSlice'
 
 import clearanceBanner from "../../assets/images/ClearanceBanner.png"
 import { RxDividerHorizontal } from "react-icons/rx"
 import Banner from "../../components/banner/Banner"
+import ProductCard from '../../components/productCard/ProductCard'
 
 import './homepage.css'
 
 function Homepage() {
   const dispatch = useDispatch()
-  const navigate = useNavigate()
   const { product, isError, message } = useSelector(state => state.product)
 
   useEffect(() => {
@@ -35,16 +33,8 @@ function Homepage() {
     swipeToSlide: true,
   }
 
-  // Retrieve selected product 
-  const retrieveProduct = (e) => {
-    dispatch(getProduct(e.target.dataset.id))
-
-    navigate(`product/${e.target.dataset.name.replace(/\s+/g, '-').toLowerCase()}`)
-  }
-
   const renderedCarousel = product.map(item =>
-    <img key={item.id} src={item.image} alt={item.description} onClick={retrieveProduct}
-    data-id={item.id} data-name={item.name} />
+    <ProductCard key={item.id} item={item} />
   )
 
   return (
